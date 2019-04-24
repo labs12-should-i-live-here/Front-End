5c7163640fe1f686dfd54707e19e064cc223c5f1
Brian Illes <brianrilles@gmail.com>
2019-04-24 15:48:26 -0700

    Pulls test data from BE

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
+import axios from "axios";
 
 function App() {
+  const [test, setTest] = useState("");
+
+  useEffect(() => {
+    const url = "https://labs12.herokuapp.com/";
+    axios
+      .get(url)
+      .then(res => {
+        setTest(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
+    // this is a test for E2E sake
+
     <div className="App">
-      <section>
-        <p>Front-end up</p>
-      </section>
+      <p>{test}</p>
     </div>
   );
 }
