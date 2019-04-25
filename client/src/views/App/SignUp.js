5c38f430fb58595c63956ab2b6ead60a1ff40a26
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-25 17:34:43 -0400

    created basic login/reg fields that share a common scss style file

--- client/src/views/App/SignUp.js
+++ client/src/views/App/SignUp.js
@@ -1,9 +1,15 @@
 import React from "react";
+import '../../scss/UserForm.scss'
 
 function SignUp() {
   return (
-    <div>
+    <div className='main'>
       <p>SignUp</p>
+      <form className='form'>
+        <input className='input' placeholder="Username"></input>
+        <input className='input' placeholder="Password"></input>
+        <input className='input' placeholder="Zipcode"></input>
+      </form>
     </div>
   );
 }
