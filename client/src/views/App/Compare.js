b5968bf341db92ff8eac5fab245e766166e78b02
Brian Illes <brianrilles@gmail.com>
2019-04-25 11:54:32 -0700

    Adds register, login, and compare routes

---
+++ client/src/views/App/Compare.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Shared/Navbar.js";
+
+function Compare() {
+  return (
+    <div>
+      <Navbar />
+      <p>Compare</p>
+    </div>
+  );
+}
+
+export default Compare;
