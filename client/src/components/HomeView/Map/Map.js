35e38c0106b99c1e0317e9280fc7b316779bac0a
Brian Illes <brianrilles@gmail.com>
2019-04-25 20:07:59 -0700

    User can view Map

--- client/src/components/HomeView/Map/Map.js
+++ client/src/components/HomeView/Map/Map.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "../../../scss/Map.scss";
-function Map() {
-  return (
-    <div className="Map">
-      <p>Map</p>
-    </div>
-  );
+import ReactMapGL from "react-map-gl";
+
+class Map extends Component {
+  state = {
+    viewport: {
+      width: "100%",
+      height: "100%",
+      latitude: 37.7749,
+      longitude: -122.4194,
+      zoom: 3
+    }
+  };
+
+  render() {
+    console.log("token", process.env.REACT_APP_MAPBOX_TOKEN);
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle={"mapbox://styles/brilles/cjuxa750e671g1fml154ev74e"}
+        onViewportChange={viewport => this.setState({ viewport })}
+      />
+    );
+  }
 }
 
 export default Map;
