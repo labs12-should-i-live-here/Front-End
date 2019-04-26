095fb5e0ab6347e4a3e66a0012a41e57e13ed258
Kim Duclos <kimduclos@gmail.com>
2019-04-26 06:52:44 -0600

    auth0 updated to hold user profiles

--- client/src/views/App/Auth/Auth.js
+++ client/src/views/App/Auth/Auth.js
@@ -6,13 +6,15 @@ export default class Auth {
 	accessToken;
 	idToken;
 	expiresAt;
+	userProfile
 
 	auth0 = new auth0.WebAuth({
 		domain: AUTH_CONFIG.domain,
 		clientID: AUTH_CONFIG.clientId,
 		redirectUri: AUTH_CONFIG.callbackUrl,
 		responseType: 'token id_token',
-		scope: 'openid'
+		scope: 'openid',
+		scope: 'openid profile'
 	});
 
 	constructor() {
@@ -23,6 +25,7 @@ export default class Auth {
 		this.getAccessToken = this.getAccessToken.bind(this);
 		this.getIdToken = this.getIdToken.bind(this);
 		this.renewSession = this.renewSession.bind(this);
+		this.getProfile = this.getProfile.bind(this);
 	}
 
 	login() {
@@ -30,7 +33,7 @@ export default class Auth {
 	}
 
 	handleAuthentication() {
-		this.auth0.parseHash((err, authResult) => {
+		this.auth0.parseHash((errir, authResult) => {
 			if (authResult && authResult.accessToken && authResult.idToken) {
 				this.setSession(authResult);
 			} else if (err) {
@@ -75,11 +78,21 @@ export default class Auth {
 		});
 	}
 
+	getProfile(cb) {
+		this.auth0.client.userInfo(this.accessToken, (err, profile) => {
+		  if (profile) {
+			this.userProfile = profile;
+		  }
+		  cb(err, profile);
+		});
+	  }
+
 	logout() {
 		// Remove tokens and expiry time
 		this.accessToken = null;
 		this.idToken = null;
 		this.expiresAt = 0;
+		this.profile = null;
 
 		// Remove isLoggedIn flag from localStorage
 		localStorage.removeItem('isLoggedIn');
