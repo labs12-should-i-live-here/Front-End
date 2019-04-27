933c7f08b98aa4a5a54526a76f904d04af8bcb9e
Brian Illes <brianrilles@gmail.com>
2019-04-27 15:07:06 -0700

    Refactors to native Mapbox API, removes react-map-gl

--- client/src/components/HomeView/Map/Map.js
+++ client/src/components/HomeView/Map/Map.js
@@ -1,52 +1,30 @@
 import React, { Component } from "react";
 import "../../../scss/Map.scss";
-import ReactMapGL, {
-  NavigationControl,
-  LinearInterpolator,
-  FlyToInterpolator
-} from "react-map-gl";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 class Map extends Component {
   state = {
-    viewport: {
-      width: "100%",
-      height: "100%",
-      latitude: 37.7749,
-      longitude: -122.4194,
-      zoom: 3
-    }
+    height: "100vh",
+    longitude: -74.006,
+    latitude: 40.7128,
+    zoom: 5
   };
 
   render() {
-    return (
-      <ReactMapGL
-        {...this.state.viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle={"mapbox://styles/brilles/cjuxa750e671g1fml154ev74e"}
-        onViewportChange={this._onViewPortChange}
-      >
-        <div className="controller">
-          <NavigationControl onViewportChange={this._onViewPortChange} />
-        </div>
-        <button onClick={this._goToAddress}>Go to Static address</button>
-      </ReactMapGL>
-    );
+    return <div id="map" />;
   }
-  _onViewPortChange = viewport => {
-    this.setState({ viewport });
-  };
+  componentDidMount() {
+    const { longitude, latitude, zoom } = this.state;
 
-  _goToAddress = () => {
-    const viewport = {
-      ...this.state.viewport,
-      longitude: -118.353996,
-      latitude: 33.919434,
-      zoom: 10,
-      transitionDuration: 5000,
-      transitionInterpolator: new FlyToInterpolator()
-    };
-    this.setState({ viewport });
-  };
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/brilles/cjuxa750e671g1fml154ev74e",
+      center: [longitude, latitude],
+      zoom
+    });
+  }
 }
 
 export default Map;
