4dcbd72cd3933c88e07b097b91f71cbbbf25432c
Kim Duclos <kimduclos@gmail.com>
2019-04-29 15:02:02 -0600

    App.js Auth0 rebuilt

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,40 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import "./scss/App.scss";
-import Home from "./views/App/Home.js";
-import SignUp from "./views/App/SignUp.js";
-import Login from "./views/App/Login.js";
-import Compare from "./views/App/Compare.js";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './scss/App.scss';
+import Home from './views/App/Home.js';
+import SignUp from './views/App/SignUp.js';
+import Login from './views/App/Login.js';
+import Compare from './views/App/Compare.js';
+import React, { Component } from 'react';
 
-// Auth 0
-import Callback from "./containers/auth-zero/Callback/Callback.js";
-import Auth from "./containers/auth-zero/Auth/Auth.js";
+// auth0
+import Callback from './containers/auth-zero/Callback/Callback.js';
+import Auth from './containers/auth-zero/Auth/Auth.js';
 
-// import StripeCallback from "./containers/stripe-callback/StripeCallback.js";
+const auth = new Auth();
+const handleAuthentication = ({ location }) => {
+	if (/access_token|id_token|error/.test(location.hash)) {
+		auth.handleAuthentication();
+	}
+};
 
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<Route exact path="/" component={Home} />
+				<Route path="/register" component={SignUp} />
+				<Route path="/login" component={Login} />
+				<Route path="/compare" component={Compare} />
 
-function App() {
-  return (
-    <>
-      <Route exact path="/" component={Home} />
-      <Route path="/register" component={SignUp} />
-      <Route path="/login" component={Login} />
-      <Route path="/compare" component={Compare} />
-    </>
-  );
+				<Route path="/callback" render={props => {
+						handleAuthentication(props);
+						return <Callback {...props} />;
+					}}
+				/>
+			</div>
+		);
+	}
 }
-
-// Auth0
 
 export default App;
