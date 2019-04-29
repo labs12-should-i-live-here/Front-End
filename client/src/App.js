3f2919f2f4db58dac4290c35f11b903f0a18d4e0
Kim Duclos <kimduclos@gmail.com>
2019-04-29 14:40:05 -0600

    removed Auth0 from App.js - crashing

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ import Compare from "./views/App/Compare.js";
 import Callback from "./containers/auth-zero/Callback/Callback.js";
 import Auth from "./containers/auth-zero/Auth/Auth.js";
 
+// import StripeCallback from "./containers/stripe-callback/StripeCallback.js";
+
+
 function App() {
   return (
     <>
@@ -22,52 +25,5 @@ function App() {
 }
 
 // Auth0
-const auth = new Auth();
-const handleAuthentication = ({ location }) => {
-  if (/access_token|id_token|error/.test(location.hash)) {
-    auth.handleAuthentication();
-  }
-};
 
-class App extends Component {
-  render() {
-    return (
-      <MuiThemeProvider theme={theme}>
-        <div className="App">
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <Suspense fallback={<Callback />}>
-                  <LazyLandingPage auth={auth} {...props} />
-                </Suspense>
-              )}
-            />
-
-            <Route
-              path="/callback"
-              render={props => {
-                handleAuthentication(props);
-                return <Callback {...props} />;
-              }}
-            />
-
-            <Route
-              path="/dashboard"
-              render={props => (
-                <Suspense fallback={<Callback />}>
-                  <Dashboard {...props} auth={auth} />
-                </Suspense>
-              )}
-            />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/stripe-callback" component={StripeCallback} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </div>
-      </MuiThemeProvider>
-    );
-  }
-}
 export default App;
