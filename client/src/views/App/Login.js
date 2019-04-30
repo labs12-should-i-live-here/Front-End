5d6753f5345e279f7693fe2ee06fd35d30c07d22
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-30 15:08:03 -0400

    hooked up login,but it still needs a login function from redux

--- client/src/views/App/Login.js
+++ client/src/views/App/Login.js
@@ -1,18 +1,55 @@
 import React from "react";
 import Navbar from "../../components/Shared/Navbar.js";
 import '../../scss/UserForm.scss'
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
-function Login() {
+var Red = styled.p`text-align:center;`
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      credentials: {
+        email: "",
+        password: ""
+      }
+    };
+  };
+
+  onChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+//need to create function for log in
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.login(this.state.credentials);
+  };
+
+  render(){
   return (
+    <>
     <div>
     <Navbar/>
-      <form className='modal'>
-        <input className='input' placeholder="Email"></input>
-        <input className='input' placeholder="Password"></input>
-        <button className="options btn">Log in</button>
+      <form className='modal' onSubmit={this.onSubmit}>
+        <input className='input' placeholder="Email" 
+          type="text" onChange={this.onChange} 
+          value={this.state.credentials.email}></input>
+        <input className='input' placeholder="Password"
+        type="password" onChange={this.onChange}></input>
+        <button type="submit" className="options btn">Log in</button>
       </form>
+      <Red>
+      Not a Member? <Link to="/register">Join Now</Link>
+      </Red>
     </div>
-  );
+    </>
+  );}
 }
 
 export default Login;
