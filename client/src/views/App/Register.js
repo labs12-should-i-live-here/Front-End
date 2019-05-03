210d9a67e1bf5252e771b2f7a2d1900dd6acf20f
Brian Illes <brianrilles@gmail.com>
2019-05-03 07:55:42 -0700

    resolving merge conflict

--- client/src/views/App/Register.js
+++ client/src/views/App/Register.js
@@ -1,76 +1,76 @@
-// auth0
-// import Auth from './Auth';
-// import { Navbar, Button } from 'react-bootstrap';
-// import './App.css';
+auth0
+import Auth from './Auth';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
 
-// const auth = new Auth();
-// auth.login();
+const auth = new Auth();
+auth.login();
 
 
-// class App extends Component {
+class App extends Component {
 
-// 	goTo(route) {
-// 		this.props.history.replace(`/${route}`)
-// 	  }
+	goTo(route) {
+		this.props.history.replace(`/${route}`)
+	  }
 	
-// 	  login() {
-// 		this.props.auth.login();
-// 	  }
+	  login() {
+		this.props.auth.login();
+	  }
 	
-// 	  logout() {
-// 		this.props.auth.logout();
-// 	  }
+	  logout() {
+		this.props.auth.logout();
+	  }
 	
-// 	  componentDidMount() {
-// 		const { renewSession } = this.props.auth;
+	  componentDidMount() {
+		const { renewSession } = this.props.auth;
 	
-// 		if (localStorage.getItem('isLoggedIn') === 'true') {
-// 		  renewSession();
-// 		}
-// 	  }
+		if (localStorage.getItem('isLoggedIn') === 'true') {
+		  renewSession();
+		}
+	  }
 	
-// 	  render() {
-// 		const { isAuthenticated } = this.props.auth;
+	  render() {
+		const { isAuthenticated } = this.props.auth;
 	
-// 		return (
-// 		  <div>
-// 			<Navbar fluid>
-// 			  <Navbar.Header>
-// 				<Navbar.Brand>
-// 				  <a href="#">Auth0 - React</a>
-// 				</Navbar.Brand>
-// 				<Button
-// 				  bsStyle="primary"
-// 				  className="btn-margin"
-// 				  onClick={this.goTo.bind(this, 'home')}
-// 				>
-// 				  Home
-// 				</Button>
-// 				{
-// 				  !isAuthenticated() && (
-// 					  <Button
-// 						bsStyle="primary"
-// 						className="btn-margin"
-// 						onClick={this.login.bind(this)}
-// 					  >
-// 						Log In
-// 					  </Button>
-// 					)
-// 				}
-// 				{
-// 				  isAuthenticated() && (
-// 					  <Button
-// 						bsStyle="primary"
-// 						className="btn-margin"
-// 						onClick={this.logout.bind(this)}
-// 					  >
-// 						Log Out
-// 					  </Button>
-// 					)
-// 				}
-// 			  </Navbar.Header>
-// 			</Navbar>
-// 		  </div>
-// 		);
-// 	  }
-// 	}
+		return (
+		  <div>
+			<Navbar fluid>
+			  <Navbar.Header>
+				<Navbar.Brand>
+				  <a href="#">Auth0 - React</a>
+				</Navbar.Brand>
+				<Button
+				  bsStyle="primary"
+				  className="btn-margin"
+				  onClick={this.goTo.bind(this, 'home')}
+				>
+				  Home
+				</Button>
+				{
+				  !isAuthenticated() && (
+					  <Button
+						bsStyle="primary"
+						className="btn-margin"
+						onClick={this.login.bind(this)}
+					  >
+						Log In
+					  </Button>
+					)
+				}
+				{
+				  isAuthenticated() && (
+					  <Button
+						bsStyle="primary"
+						className="btn-margin"
+						onClick={this.logout.bind(this)}
+					  >
+						Log Out
+					  </Button>
+					)
+				}
+			  </Navbar.Header>
+			</Navbar>
+		  </div>
+		);
+	  }
+	}
