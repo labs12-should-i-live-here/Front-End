d417ac31dd4e06ab66c9ce0d9dbf65cae0b364f1
Brian Illes <brianrilles@gmail.com>
2019-05-06 12:28:03 -0700

    removes extraneous login methods

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,40 +1,6 @@
 import axios from "axios";
 
-//login actions
-export const LOGIN_START = "LOGIN_START";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
-
-export const login = creds => dispatch => {
-  dispatch({ type: LOGIN_START });
-  return axios
-    .post("https://labs12.herokuapp.com/login", creds)
-    .then(res => {
-      console.log(res);
-      // localStorage.setItem("token", res.data.payload);
-      // dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload });
-    })
-    .catch(err => console.log(err));
-};
-
-//sign up actions
-export const SIGN_UP_START = "SIGN_UP_START";
-export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
-export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
-
-export const signUp = creds => dispatch => {
-  dispatch({ type: SIGN_UP_START });
-  return axios
-    .post("https://labs12.herokuapp.com/register", creds)
-    .then(res => {
-      console.log(res);
-      // localStorage.setItem("token", res.data.payload);
-      // dispatch({ type: SIGN_UP_SUCCESS, payload: res.data.payload });
-    })
-    .catch(err => console.log(err.response));
-};
-
-// Todo: fetching predictions for a coordinate. Displaying that data on the plotly.
+// Todo: Displaying that data on the plotly.
 export const FETCH_PREDICTION_DATA_START = "FETCH_PREDICTION_DATA_START";
 export const FETCH_PREDICTION_DATA_SUCCESS = "FETCH_PREDICTION_DATA_SUCCESS";
 export const FETCH_PREDICTION_DATA_FAILURE = "FETCH_PREDICTION_DATA_FAILURE";
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,28 +1,10 @@
 import {
-  LOGIN_START,
-  LOGIN_SUCCESS,
-  // LOGIN_FAILURE,
-  // SIGN_UP_START,
-  // SIGN_UP_SUCCESS,
-  // SIGN_UP_FAILURE,
   FETCH_PREDICTION_DATA_START,
   FETCH_PREDICTION_DATA_SUCCESS,
   FETCH_PREDICTION_DATA_FAILURE
 } from "../actions/index";
 
 const initialState = {
-  credentials: {
-    username: "",
-    password: ""
-  },
-  signUp: {
-    error: "",
-    signingUp: false
-  },
-  logIn: {
-    error: "",
-    loggingIn: false
-  },
   fetchingPredictionData: false,
   error: "",
   errorStatusCode: null,
@@ -31,18 +13,6 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN_START:
-      return {
-        ...state,
-        error: "",
-        loggingIn: true
-      };
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        error: "",
-        loggingIn: false
-      };
     case FETCH_PREDICTION_DATA_START:
       return {
         ...state,
@@ -56,6 +26,7 @@ const reducer = (state = initialState, action) => {
       };
     case FETCH_PREDICTION_DATA_FAILURE:
       return {
+        ...state,
         error: action.payload.status,
         fetchingPredictionData: false,
         errorStatusCode: action.payload.status
