d028699ea8a3501e0103b4c1f06b991bf230c3e9
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-05-06 15:06:56 -0400

    Populated compare view and imports. Created TwinMap.js to display two maps

--- client/src/views/App/Compare.js
+++ client/src/views/App/Compare.js
@@ -1,14 +1,32 @@
 import React from "react";
 import Navbar from "../../components/Shared/Navbar.js";
 import Footer from "../../components/HomeView/Footer";
+import TwinMap from "../../components/CompareView/TwinMap";
+import Stats from "../../components/HomeView/Stats/Stats.js";
+import CompareDeck from "../../components/HomeView/Compare/CompareDeck.js";
+import "../../scss/Compare.scss"
 
 function Compare() {
   return (
     <div>
       <Navbar />
-      <p>Compare</p>
-      <Footer />
-    </div>
+      <div className="main-content">
+          <div className="left-panel">
+            <TwinMap />
+          </div>
+          <div className="right-panel">
+            <div className="top-panel">
+              <Stats />
+            </div>
+            <div className="bottom-panel">
+              <CompareDeck />
+            </div>
+          </div>
+        </div>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
   );
 }
 
