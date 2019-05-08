9b0ac08171e31b3e8a145abb405f819af810d788
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-05-08 02:28:41 -0400

    added materal ui scss core

--- splash/src/App.js
+++ splash/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router";
+
+import "assets/scss/material-kit-pro-react.scss?v=1.3.0";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">hi</header>
     </div>
   );
 }
