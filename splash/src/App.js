242214f63f7b6c725abda854889dd3b1978bbdaf
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-05-08 03:51:09 -0400

    created landing page skeleton from materal ui template, this is not how it will look in the final this is an example buildt with material ui

--- splash/src/App.js
+++ splash/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 
 import { createBrowserHistory } from "history";
-let classic = createBrowserHistory();
 
-import "assets/scss/material-kit-pro-react.scss?v=1.3.0";
+import "./assets/scss/material-kit-pro-react.scss";
 
 import { Router, Route, Switch } from "react-router";
-import SplashPage from "views/LandingPage/LandingPage.jsx";
+import SplashPage from "./views/LandingPage/LandingPage";
 
-export default function App() {
+let classic = createBrowserHistory();
+function App() {
   return (
     <Router history={classic}>
       <Switch>
@@ -18,3 +18,5 @@ export default function App() {
     </Router>
   );
 }
+
+export default App;
