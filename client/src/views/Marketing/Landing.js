a38f731e85b74432942bffb5e08dc043108ef57d
Kim Duclos <kimduclos@gmail.com>
2019-05-08 21:21:27 -0600

    commiting to pull

---
+++ client/src/views/Marketing/Landing.js
@@ -0,0 +1,11 @@
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
+import { Component } from 'react';
+
+class Landing extends Component{
+    render() {
+        return (
+            <Navbar />
+        )
+    }
+}
