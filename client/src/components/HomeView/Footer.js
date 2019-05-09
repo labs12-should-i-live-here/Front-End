dfc4553a520b537b7beee2b67014e2be7f77b95d
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-05-09 15:20:19 -0400

    made changes to the footer

--- client/src/components/HomeView/Footer.js
+++ client/src/components/HomeView/Footer.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react'
+
+
 import React, { Component } from "react";
 import styled from "styled-components";
 import { IoLogoGithub } from "react-icons/io";
@@ -14,23 +17,25 @@ var B = styled.a`
 `;
 
 var Div = styled.div`
-border:solid red 1px`;
+border:solid red 1px;`;
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <Div>
-        <Foot>
-          <nav>
-            <a href="https://github.com/labs12-should-i-live-here/Front-End">
-              <IoLogoGithub />
-            </a>
-            <B>sitemap</B>
-          </nav>
-        </Foot>
-      </Div>
-    );
+  export default class Footer extends Component {
+    render() {
+      return (
+        <Div>
+          <Foot>
+            <nav>
+              <a href='https://github.com/labs12-should-i-live-here/Front-End'>
+                <IoLogoGithub />
+              </a>
+              <B>sitemap</B>
+            </nav>
+          </Foot>
+        </Div>
+      )
+    }
   }
-}
+
+
 
 //react-router-sitemap might be the best option for a sitemap generator. Sitemaps help with the SEO
