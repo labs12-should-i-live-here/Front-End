d76e672b6a7fb6c017a5bef1dddea8b434febf05
Kim Duclos <kimduclos@gmail.com>
2019-05-16 14:31:01 -0600

    about.js skeleton

--- client/src/views/App/Landing.js
+++ client/src/views/App/Landing.js
@@ -22,6 +22,9 @@ class Landing extends Component {
                             <div className='subtagline'>
                                 We'll help you find a safe home
                             </div>
+                            <div className='btnBox'>
+                            <button className='btn'>Counties</button>
+                        </div>   
                             <div id="geocoder" className='landingSearchBar'></div>
                         </div>
 
@@ -43,8 +46,19 @@ class Landing extends Component {
                             </div>
                             <div className="textBox">
                                 Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam 
+        rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores 
+        eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, 
+        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam 
+        aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit 
+        laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea 
+        voluptate velit esse quam nihil molestiae 
                         </div>
+                        <div className='btnBox'>
+                            <button className='btn'>Heat Maps</button>
+                        </div>    
+
                         </div>
                         <div className="infoBox">
                             <div className="infoImage">
@@ -53,8 +67,18 @@ class Landing extends Component {
                             </div>
                             <div className="textBox">
                                 1914 translation by H. Rackham
-        "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+        "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and 
+        I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, 
+        the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, 
+        but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. 
+        Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because 
+        occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, 
+        which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right 
+        to find fault with a man who
                         </div>
+                        <div className='btnBox'>
+                            <button className='btn'>Earthquakes</button>
+                        </div>    
                         </div>
                         <div className="infoBox">
                             <div className="infoImage">
@@ -63,8 +87,19 @@ class Landing extends Component {
                             </div>
                             <div className="textBox">
                                 Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
+        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt 
+        mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, 
+        cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda 
+        est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates 
+        repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
+        alias uatur aut perferendi
                         </div>
+                         <div className='btnBox'>
+                            <button className='btn'>Counties</button>
+                        </div>    
+
+
                         </div>
                     </div>
                     <div style={{ display: "none" }}>
