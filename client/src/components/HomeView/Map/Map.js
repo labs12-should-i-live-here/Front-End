def47f543d9ecfa2a074a2bdfb7aed3342f6518a
Ning Yi <ialeonard@ucdavis.edu>
2019-05-17 15:40:52 -0700

    removed head from maps.js

--- client/src/components/HomeView/Map/Map.js
+++ client/src/components/HomeView/Map/Map.js
@@ -715,7 +715,6 @@ class Map extends Component {
       //   },
       //   "filter": ["in", "COUNTY", ""]
       //   }, 'settlement-label'); // Place polygon under these labels.
-<<<<<<< HEAD
       map.addLayer({
         "id": "total-risk",
         "type": "fill",
@@ -758,48 +757,6 @@ class Map extends Component {
 
 
       console.log('pins from inside playback function ', pins, pins.length);
-=======
-      map.addLayer(
-        {
-          id: "total-risk",
-          type: "fill",
-          source: "totalrisk",
-          "source-layer": "danger-8xjejj",
-
-          //         "interpolate",
-          // ["exponential", 0.5],
-          // ["zoom"],
-          // 15,
-          // "#e2714b",
-          // 22,
-          // "#eee695"
-          //'filter': ['==', 'isCounty', true],
-          paint: {
-            "fill-color": {
-              property: "danger",
-              stops: [[0, "#F0334C"], [500, "#FB1"], [1000, "#82F570"]]
-            },
-            "fill-opacity": 0.35
-          }
-          // "paint": {
-          //   'paint': {
-          //     'fill-color': [
-          //     'interpolate',
-          //     ['linear'],
-          //     ['get', 'danger'],
-          //     0, '#F2F12D',
-          //     250, '#EED322',
-          //     450, '#E6B71E',
-          //     ],
-          //     'fill-opacity': 0.75
-          //     }
-          // }
-        },
-        "settlement-label"
-      ); // Place polygon under these labels.
-
-      console.log("pins from inside playback function ", pins, pins.length);
->>>>>>> 91cd0606688709c389f36ef3a610cd98bfa86126
 
       // map.addLayer({
       //   "id": "highlight",
