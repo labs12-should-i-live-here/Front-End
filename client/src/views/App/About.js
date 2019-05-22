9e86cc1e60b6429fbab42209b12af1a96370d048
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-05-22 09:07:27 -0700

    fixed aboutjs

--- client/src/views/App/About.js
+++ client/src/views/App/About.js
@@ -1,24 +1,33 @@
-// import React, { Component } from "react";
-// import NavBarB from "../../../components/Shared/NavbarB.js";
-// import Search from "../../../components/LandingSearch/Search.js";
-// import Map from "../../../components/HomeView/Map/Map.js";
-// import Component1 from "../../../components/AboutView/Component1.js";
-// import Component2 from "../../../components/AboutView/Component2.js";
+import React, { Component } from "react";
+import NavBarB from "../../components/Shared/NavbarB.js";
+import Search from "../../components/LandingSearch/Search.js";
+import Map from "../../components/HomeView/Map/Map.js";
+import Component1 from "../../components/AboutView/Component1.js";
+import Component2 from "../../components/AboutView/Component2.js";
 
-// class About extends Component {
-//   render() {
-//     return (
-//       <div>
-       
-//               </div>
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <NavBarB />
+        <div style={{ display: "none" }}>
+          <Map />
+        </div>
+        <div className="container">
+          <Component1 />
+          <Component2 />
+          <div className="header" />
+          <div id="main">
+            <div className="inner">
+              <div className="section" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-//               <footer />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+export default About;
 
-// export default About;
+//lol
