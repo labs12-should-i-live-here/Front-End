7e4f7060e8f0a5006d0a7de46f6d97b52babc605
Ning Yi <ialeonard@ucdavis.edu>
2019-05-24 13:09:25 -0700

    fixed auth0 error

--- client/src/Auth0/Auth.js
+++ client/src/Auth0/Auth.js
@@ -130,11 +130,11 @@ export default class Auth {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
       } else if (err) {
-        this.logout();
-        console.log(err);
-        alert(
-          `Could not get a new token (${err.error}: ${err.error_description}).`
-        );
+      //   this.logout();
+      //   console.log(err);
+      //   alert(
+      //     `Could not get a new token (${err.error}: ${err.error_description}).`
+      //   );
       }
     });
   }
